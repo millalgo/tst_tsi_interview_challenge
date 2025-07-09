f09c61bd8f1eb2e5157c263615388b7e32c70e47
Don Miller <don.miller@vantagehilltech.com>
2025-07-08 21:08:38 -0400

    1.2 Output key metrics

--- main.py
+++ main.py
@@ -1,4 +1,11 @@
 import pandas as pd
 
 # 1.1 Ingest the data into Python
-df = pd.read_csv('2024-04-18_Ancient_Tree_Inventory.csv')
+df = pd.read_csv('data/2024-04-18_Ancient_Tree_Inventory.csv')
+
+# 1.2 Output number of rows
+# https://www.geeksforgeeks.org/python/count-the-number-of-rows-and-columns-of-a-pandas-dataframe/
+rows, columns = df.shape
+
+print(f'Count/size:       {rows}')
+print(f'Number of fields: {columns}')
